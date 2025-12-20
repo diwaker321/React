004b3d86297d379efbca6edec6bf8483701e936a
diwaker321 <diwakeracadmic@gmail.com>
2025-12-20 02:48:48 -0800

    commit done

--- helloworld/igniteOurApp/app.js
+++ helloworld/igniteOurApp/app.js
@@ -1,3 +1,7 @@
-const root = ReactDOM.createRoot(document.getElementById('root'))
-const element = React.createElement('h1',{},'hello coders')
-root.render(element)
+import React from "react";
+import ReactDOM from "react-dom/client"; // dont use only react-dom else it will give an error of (createRoot is not a function)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const element = React.createElement("h1",
+     {}, "hello coders ");
+root.render(element);
